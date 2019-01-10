fd0374b87088eb8d22370e6f7ce1aded6663ddf2
paredfed <paredfed@gmail.com>
2019-01-09 18:26:03 -0600

    Updates

---
+++ precache-manifest.e88bddcdfd20d03acbae4f0f593e5d4e.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "6acb6692e4ee25941da6",
+    "url": "/clicky-game/static/js/runtime~main.6acb6692.js"
+  },
+  {
+    "revision": "896c06324818bde3da68",
+    "url": "/clicky-game/static/js/main.896c0632.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "/clicky-game/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "060c188138f6761e01e938d7bcf0d06e",
+    "url": "/clicky-game/index.html"
+  }
+];
